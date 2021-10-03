fb720f365d9cd38b5af25cf54b2bf65519d3ff06
ajain4743 <ajain4743@conestogac.on.ca>
2021-10-03 17:22:25 -0400

    Add project files.

---
+++ AJClubs/Controllers/AJInstrumentsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AJClubs.Models;
+
+namespace AJClubs.Controllers
+{
+    public class AJInstrumentsController : Controller
+    {
+        private readonly AJClubsContext _context;
+
+        public AJInstrumentsController(AJClubsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AJInstruments
+        // return list of all the instruments in DB
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Instrument.ToListAsync());
+        }
+
+        // GET: AJInstruments/Details/5
+        // return the details of a selected instrument
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var instrument = await _context.Instrument
+                .FirstOrDefaultAsync(m => m.InstrumentId == id);
+            if (instrument == null)
+            {
+                return NotFound();
+            }
+
+            return View(instrument);
+        }
+
+        // GET: AJInstruments/Create
+        // create a new instrument
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AJInstruments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("InstrumentId,Name")] Instrument instrument)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(instrument);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(instrument);
+        }
+
+        // GET: AJInstruments/Edit/5
+        // edit the details of an existing instrument
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var instrument = await _context.Instrument.FindAsync(id);
+            if (instrument == null)
+            {
+                return NotFound();
+            }
+            return View(instrument);
+        }
+
+        // POST: AJInstruments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("InstrumentId,Name")] Instrument instrument)
+        {
+            if (id != instrument.InstrumentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(instrument);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InstrumentExists(instrument.InstrumentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(instrument);
+        }
+
+        // GET: AJInstruments/Delete/5
+        // delete an existing instrument
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var instrument = await _context.Instrument
+                .FirstOrDefaultAsync(m => m.InstrumentId == id);
+            if (instrument == null)
+            {
+                return NotFound();
+            }
+
+            return View(instrument);
+        }
+
+        // POST: AJInstruments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var instrument = await _context.Instrument.FindAsync(id);
+            _context.Instrument.Remove(instrument);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InstrumentExists(int id)
+        {
+            return _context.Instrument.Any(e => e.InstrumentId == id);
+        }
+    }
+}
---
+++ AJClubs/Controllers/AJStylesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AJClubs.Models;
+
+namespace AJClubs.Controllers
+{
+    public class AJStylesController : Controller
+    {
+        private readonly AJClubsContext _context;
+
+        public AJStylesController(AJClubsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AJStyles
+        // return list of all the styles in DB
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Style.ToListAsync());
+        }
+
+        // GET: AJStyles/Details/5
+        // return details of selected style
+
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var style = await _context.Style
+                .FirstOrDefaultAsync(m => m.StyleName == id);
+            if (style == null)
+            {
+                return NotFound();
+            }
+
+            return View(style);
+        }
+
+        // GET: AJStyles/Create
+        // create new style
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AJStyles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("StyleName,Description")] Style style)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(style);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(style);
+        }
+
+        // GET: AJStyles/Edit/5
+        // edit the details if an existing style
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var style = await _context.Style.FindAsync(id);
+            if (style == null)
+            {
+                return NotFound();
+            }
+            return View(style);
+        }
+
+        // POST: AJStyles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("StyleName,Description")] Style style)
+        {
+            if (id != style.StyleName)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(style);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StyleExists(style.StyleName))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(style);
+        }
+
+        // GET: AJStyles/Delete/5
+        // delete an existing style
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var style = await _context.Style
+                .FirstOrDefaultAsync(m => m.StyleName == id);
+            if (style == null)
+            {
+                return NotFound();
+            }
+
+            return View(style);
+        }
+
+        // POST: AJStyles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var style = await _context.Style.FindAsync(id);
+            _context.Style.Remove(style);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StyleExists(string id)
+        {
+            return _context.Style.Any(e => e.StyleName == id);
+        }
+    }
+}
